3235d723386ef85fc3903a1f7e06ae6e4a8a58a7
Bill Ray <bill.ray0259@gmail.com>
2025-01-14 18:01:53 -0700

    naiop-challenge map

--- geojson_mapper/layout.py
+++ geojson_mapper/layout.py
@@ -1,32 +1,54 @@
 # dash_app/layout.py
 
 import dash_bootstrap_components as dbc
-from dash import html, dcc
 import dash_leaflet as dl
+import json
+from shapely.geometry import shape
+from dash import dcc
 
 def create_layout():
+    
+    with open('map.geojson') as f:
+        geojson = json.load(f)
+    
+        geojson_layer = []
+        centroids = []
+        latitudes = []
+        longitudes = []
+        
+        for feature in geojson['features']:
+            feature_component = dl.GeoJSON(
+                data=feature,
+                children=dl.Tooltip(content=feature['properties']['description'])
+            )
+            if 'icon' in feature['properties']:
+                feature_component = dl.Marker(
+                    position=[feature['geometry']['coordinates'][1], feature['geometry']['coordinates'][0]],
+                    children=[
+                        dl.Tooltip(content=feature['properties']['name']),
+                        dl.Popup(content=feature['properties']['description'])
+                    ],
+                    icon=feature['properties']['icon']
+                )
+            geojson_layer.append(
+                feature_component
+            )
+            geom = shape(feature['geometry'])
+            centroids.append(geom.centroid)
+            bounds = geom.bounds
+            longitudes.extend([bounds[0], bounds[2]])
+            latitudes.extend([bounds[1], bounds[3]])
+        
+        avg_lat = sum(centroid.y for centroid in centroids) / len(centroids)
+        avg_lon = sum(centroid.x for centroid in centroids) / len(centroids)
+    
     return dbc.Container([
         dbc.Row([
             dbc.Col([
-                dcc.Upload(
-                    id='upload-data',
-                    children='Drag and Drop or Click to Select a File',
-                    style={
-                        'width': '100%',
-                        'height': '60px',
-                        'lineHeight': '60px',
-                        'borderWidth': '1px',
-                        'borderStyle': 'dashed',
-                        'borderRadius': '5px',
-                        'textAlign': 'center',
-                        'margin': '10px'
-                    },
-                    multiple=False
-                ),
-                dl.Map(id='map', style={'width': '100%', 'height': '750px'}, center=[37.0902, -95.7129], zoom=4, children=[
+                dl.Map(id='map', style={'width': '100vw', 'height': '100vh'}, center=[39.8575144983084, -104.97617052195768], zoom=12, children=[
                     dl.TileLayer(),
-                    dl.LayerGroup(id='geojson-layer')
+                    dl.LayerGroup(id='geojson-layer', children=geojson_layer)
                 ])
             ])
-        ])
-    ])
+        ], style={'margin': '0', 'padding': '0'})
+    ], fluid=True, style={'margin': '0', 'padding': '0'})
